0bb263887583db6bc9ff037eff2f4f0b9848912f
Gene Piki <pikigene01@gmail.com>
2023-04-14 01:22:31 +0200

    riskcurbvueplugin

---
+++ README.md
@@ -0,0 +1,40 @@
+# WP Plugin Vue Boilerplate (Vite Build)
+This is a Customizable sample WordPress Plugin which is developed as a single page app on backend with Vue js and Tailwind custom build css. and you don't have to reload page all the time.
+
+
+[![short demo](https://img.youtube.com/vi/VA3G8ahoHLE/sddefault.jpg)](https://www.youtube.com/watch?v=VA3G8ahoHLE)
+
+
+
+<img src="./src/github-images/dashboard.png" />
+
+Caption: <i>Dummy dashboard with custom build vue + tailwind setup (Vite realtime environment)</i><br/>
+
+# How to use ?
+
+- Just clone/fork this repository
+- Check the package.json file
+- command: `npm i`
+- command: `npm run watch` for development and for production: `npm run production`
+
+On production you only need
+- assets
+- includes
+- wp-plugin-with-vue-tailwind.php (plugin Entry file)
+
+
+
+If you face any issue feel free to let me know. :)
+
+<br/>
+
+## Vue + Element UI auto command boilerplate
+<hr/>
+You can check another boilerplate plugin with vue js and element UI, You can create your own project using simple command line on that project within 2 mint.
+
+Check it here: https://github.com/hasanuzzamanbe/wp-boilerplate-plugin-with-vuejs
+
+### Other Setups you may Use
+* WordPress Plugin with Vue 3, tailwind (Laravel Mix Build) [https://github.com/hasanuzzamanbe/wp-plugin-with-vue-tailwind]
+* WordPress Plugin with Vue 2, Element UI (Laravel Mix Build) [https://github.com/hasanuzzamanbe/wp-boilerplate-plugin-with-vuejs]
+
---
+++ riskcurb.php
@@ -0,0 +1,122 @@
+<?php
+
+/**
+ * Plugin Name: WP with vue-tailwind-vite
+ * Plugin URI: http://wpminers.com/
+ * Description: A sample Wordpress plugin to implement Vue with tailwind.
+ * Author: Hasanuzzaman Shamim
+ * Author URI: http://hasanuzzaman.com/
+ * Version: 1.0.5
+ */
+define('WPM_URL', plugin_dir_url(__FILE__));
+define('WPM_DIR', plugin_dir_path(__FILE__));
+
+define('WPM_VERSION', '1.0.5');
+
+// This will automatically update, when you run dev or production
+define('WPM_DEVELOPMENT', 'yes');
+
+class WPPluginWithVueTailwind {
+    public function boot()
+    {
+        $this->loadClasses();
+        $this->registerShortCodes();
+        $this->ActivatePlugin();
+        $this->renderMenu();
+        $this->registerHooks();
+    }
+    public function registerHooks()
+    {
+        add_filter('script_loader_tag', array($this, 'addModuleToScript'), 10, 3);
+    }
+
+    public function loadClasses()
+    {
+        require WPM_DIR . 'includes/autoload.php';
+    }
+
+    public function renderMenu()
+    {
+        add_action('admin_menu', function () {
+            if (!current_user_can('manage_options')) {
+                return;
+            }
+            global $submenu;
+        $icon_url = plugins_url( '/includes/icons/logo.jpg', __FILE__ );
+
+            add_menu_page(
+                'RiskManagementapp',
+                'RiskCurb',
+                'manage_options',
+                'riskcurb.php',
+                array($this, 'renderAdminPage'),
+                $icon_url,
+                1
+            );
+            $submenu['riskcurb.php']['dashboard'] = array(
+                'Dashboard',
+                'manage_options',
+                'admin.php?page=riskcurb.php#/',
+            );
+            $submenu['riskcurb.php']['contact'] = array(
+                'Contact',
+                'manage_options',
+                'admin.php?page=riskcurb.php#/contact',
+            );
+        });
+    }
+
+    public function addModuleToScript($tag, $handle, $src)
+    {
+        if ($handle === 'WPWVT-script-boot') {
+            $tag = '<script type="module" id="WPWVT-script-boot" src="' . esc_url($src) . '"></script>';
+        }
+        return $tag;
+    }
+
+    public function renderAdminPage()
+    {
+        $loadAssets = new \WPPluginWithVueTailwind\Classes\LoadAssets();
+        $loadAssets->enqueueAssets();
+
+        $WPWVT = apply_filters('WPWVT/admin_app_vars', array(
+            'assets_url' => WPM_URL . 'assets/',
+            'ajaxurl' => admin_url('admin-ajax.php')
+        ));
+
+        wp_localize_script('WPWVT-script-boot', 'WPWVTAdmin', $WPWVT);
+
+        echo '<div class="WPWVT-admin-page" id="WPWVT_app">
+            <div class="main-menu text-white-200 bg-wheat-600 p-4">
+                <router-link to="/">
+                    Home
+                </router-link> |
+                <router-link to="/contact" >
+                    Contacts
+                </router-link>
+            </div>
+            <hr/>
+            <router-view></router-view>
+        </div>';
+    }
+
+    public function registerShortCodes()
+    {
+        // your shortcode here
+    }
+
+    public function ActivatePlugin()
+    {
+        //activation deactivation hook
+        register_activation_hook(__FILE__, function ($newWorkWide) {
+            require_once(WPM_DIR . 'includes/Classes/Activator.php');
+            $activator = new \WPPluginWithVueTailwind\Classes\Activator();
+            $activator->migrateDatabases($newWorkWide);
+        });
+    }
+}
+
+(new WPPluginWithVueTailwind())->boot();
+
+
+
